39188143cc6fb69efef36247c789f959a6d442db
Himanshi1405 <himanshisingla2005@gmail.com>
2025-09-15 07:27:35 +0530

    Update 2.cpp

--- Assignment 5/2.cpp
+++ Assignment 5/2.cpp
@@ -1,6 +1,7 @@
-/*Write a program to find the middle of a linked list.
-Input: 1->2->3->4->5
-Output- 3*/
+/*Write a program to count the number of occurrences of a given key in a singly linked
+list and then delete all the occurrences. For example, if given linked list is 1->2->1-
+>2->1->3->1 and given key is 1, then output should be 4. After deletion of all the
+occurrences of 1, the linked list is 2->2->3*/
 #include <iostream>
 using namespace std;
 
